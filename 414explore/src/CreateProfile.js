b34602f92dd4a4d6a4e84bb067c948f75279fb1d
Davis <davisbr@msoe.edu>
2023-11-21 15:10:50 -0600

    appropriate files added

---
+++ 414explore/src/CreateProfile.js
@@ -0,0 +1,13 @@
+import './CreateProfile.css';
+
+function CreateProfile() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default CreateProfile;
