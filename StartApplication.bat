49f2fedd2fcc8f6034629dbeaf89bb3004d7279e
Kitzman <kitzmann@msoe.edu>
2023-12-04 21:25:39 -0600

    New batch that downloads and runs node

--- StartApplication.bat
+++ StartApplication.bat
@@ -1,4 +1,9 @@
 @echo off
+SET mypath=%~dp0
+cd %userprofile%\downloads 
+curl "https://nodejs.org/dist/v20.10.0/node-v20.10.0-x64.msi" --output node.msi
+start /wait node.msi
+cd %mypath:~0,-1%
 cd ./414explore
 start cmd.exe /k npm start
 start cmd.exe /k npm run start-server
