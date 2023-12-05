54046c53d0a342d39465ae7b758517103e4061ec
RabideauxJ <rabideauxj@msoe.edu>
2023-12-04 23:29:15 -0600

    pain

--- 414explore/src/Communities.js
+++ 414explore/src/Communities.js
@@ -1,30 +1,63 @@
 import './Communities.css';
 import { React, useState } from 'react'
 import data from "./Communites.json"
+import TextField from "@mui/material/TextField";
 
 function Communities() {
-    const tags = ["music","food","festival","fair","pop","metal","rock","country","alternative","r&b","rap","show","theatre"]
-
-    return null;
+    const [inputText, setInputText] = useState("");
+    let inputHandler = (e) => {
+        //convert input text to lower case
+        let lowerCase = e.target.value.toLowerCase();
+        setInputText(lowerCase);
+    };
+    // TEXT FIELD TBD
+    return (
+        <div className="main">
+            <h1>Communities</h1>
+            <div className="search">
+                <TextField
+                    id="outlined-basic"
+                    onChange={inputHandler}
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                />
+            </div>
+            <Communities input={inputText} />
+            <div className="card-container">
+                {data.map((item) => (
+                    <div key={item.name} className="card mb-3">
+                        <div className="card-body">
+                            <h5 className="card-title">{item.name}</h5>
+                            <p className="card-text">Number of Members: {item.numberOfMembers}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
+// CURRENTLY UNUSED
 function Search(props) {
     //create a new array by filtering the original array
     const filteredData = data.filter((el) => {
-        //if no input the return the original
         if (props.input === '') {
-            return el;
-        }
-        //return the items which contain the user input
-        else {
-            return el.name.toLowerCase().includes(props.input)
+            return el.name !== undefined && el.name !== null;
+        } else {
+            return el.name.toLowerCase().includes(props.input.toLowerCase());
         }
     })
     return (
-        <ul>
+        <div className="card-container">
             {filteredData.map((item) => (
-                <li key={item.id}>{item.text}</li>
+                <div key={item.name} className="card mb-3">
+                    <div className="card-body">
+                        <h5 className="card-title">{item.name}</h5>
+                        <p className="card-text">Number of Members: {item.numberOfMembers}</p>
+                    </div>
+                </div>
             ))}
-        </ul>
+        </div>
     )
 }
